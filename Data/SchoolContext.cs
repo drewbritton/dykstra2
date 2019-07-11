5a840119542601215c129be55301dab47f4555fc
Drew Britton <abritton@uw.edu>
2019-07-10 23:49:58 -0700

    Completed the first part of the school context--working on the database initializer class.

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -18,5 +18,11 @@ namespace ContosoUniversity.Data {
         public DbSet<Enrollment> Enrollments { get; set; }
 
         public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
